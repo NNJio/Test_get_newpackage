9de18610c4a6b41be742d9ca359dc0498d13ef76
Phatsakorn Chosungnoen <phatsakorn.newz@gmail.com>
2022-09-29 17:17:15 +0700

    [ADD] add date time picker

---
+++ lib/models/ffb_date_time_picker.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+
+class DTP extends StatelessWidget {
+  const DTP({
+    Key? key,
+    required GlobalKey<FormBuilderState> formKey,
+  }) : _formKey = formKey, super(key: key);
+
+  final GlobalKey<FormBuilderState> _formKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return FormBuilderDateTimePicker(
+      name: 'date',
+      initialEntryMode: DatePickerEntryMode.calendar,
+      initialValue: DateTime.now(),
+      inputType: InputType.both,
+      decoration: InputDecoration(
+        labelText: 'Appointment Time',
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.close),
+          onPressed: () {
+            _formKey.currentState!.fields['date']?.didChange(null);
+          },
+        ),
+      ),
+      initialTime: const TimeOfDay(hour: 8, minute: 0),
+    );
+  }
+}
